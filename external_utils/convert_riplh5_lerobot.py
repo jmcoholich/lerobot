a6e3f5412fc03d07c5d60b02f769e5d56db0c2ce
Jeremiah Coholich <jcoholich@gatech.edu>
2026-03-06 13:10:38 -0500

    fix quats in lerobot dataset creation

--- external_utils/convert_riplh5_lerobot.py
+++ external_utils/convert_riplh5_lerobot.py
@@ -5,8 +5,8 @@ from lerobot.datasets.lerobot_dataset import LeRobotDataset
 from tqdm import tqdm
 
 # Configuration
-REPO_ID = "lerobot/eve_blocks_cartesian_triple"
-DATASET_NAME = "eve_blocks_cartesian_triple"
+REPO_ID = "lerobot/eve_blocks_cartesian_triple_fix_quat"
+DATASET_NAME = "eve_blocks_cartesian_triple_fix_quat"
 ORIG_DATASET_PATH = Path("/home/jeremiah/openteach/extracted_data/eve_blocks/h5_files")
 FPS = 20
 ROOT_DIR = Path(f"/data3/lerobot_data/{DATASET_NAME}")  # Where the dataset will be created locally
@@ -61,7 +61,9 @@ def main():
         with h5py.File(h5_path, "r") as f:
             # Extract hdf5 data
 
-            actions = np.concatenate([f['cartesian_pose_cmd'], np.expand_dims(f['gripper_action'],axis=1)], axis=1, dtype=np.float32)
+            # fix quat
+            fixed_cartesian_pose_cmd = fix_quat(f['cartesian_pose_cmd'][:])
+            actions = np.concatenate([fixed_cartesian_pose_cmd, np.expand_dims(f['gripper_action'], axis=1)], axis=1, dtype=np.float32)
             observation_states = np.concatenate([f['joint_pos'], np.expand_dims(f['gripper_state'], axis=1)], axis=1, dtype=np.float32)
             rgb_frames = f['rgb_frames'][:]
             camera_side_imgs = rgb_frames[:, 0, :, :, ::-1]
@@ -105,6 +107,12 @@ def main():
         f"Final counts: episodes={dataset.num_episodes}, frames={dataset.num_frames}. "
         "LeRobot stores many episodes inside shared parquet/mp4 files by default."
     )
+
+
+def fix_quat(pose_action):
+    idcs = pose_action[:, 3] < 0.0
+    pose_action[idcs, 3:7] *= -1
+    return pose_action
 
 
 def get_task_instructions(fname):
