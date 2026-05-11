7a53557cf39b09cf5792b0ca778eca37dd9ce3ef
Jeremiah Coholich <jcoholich@gatech.edu>
2026-05-11 16:35:13 -0400

    get rid of CUDA_VISIBLE_DEVICES after gpu upgrade

--- pi_05_inference.bash
+++ pi_05_inference.bash
@@ -2,7 +2,6 @@ rm -rf /home/jeremiah/.cache/huggingface/lerobot/dummy
 
 RECORD_NAME="${1:-last_recording}"
 
-export CUDA_VISIBLE_DEVICES=1
 export PYTHONPATH="/home/jeremiah/openteach:${PYTHONPATH}"
 
 python src/lerobot/scripts/lerobot_record.py \
@@ -13,11 +12,11 @@ python src/lerobot/scripts/lerobot_record.py \
   --dataset.push_to_hub=false \
   --dataset.root='/home/jeremiah/.cache/huggingface/lerobot/dummy' \
   --dataset.repo_id=dummy/eval_dummy \
-  --dataset.single_task="Plug the charger into the power strip" \
+  --dataset.single_task="Unscrew the nut and set it on the table" \
   --dataset.episode_time_s=60000 \
   --dataset.num_episodes=1 \
   --policy.dtype=bfloat16 \
-  --policy.path=/home/jeremiah/lerobot/outputs/plug3/checkpoints/003000/pretrained_model
+  --policy.path=/home/jeremiah/lerobot/outputs/thread3_unthread4_bc_and_dagger_12k_4GPU_double_lr/checkpoints/006000/pretrained_model
   # --policy.n_action_steps=20
   # --dataset.single_task="Plug the charger into the power strip" \
   # --policy.type=pi05 \
